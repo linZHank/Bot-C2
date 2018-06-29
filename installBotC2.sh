9623202cf145e03c77799dff6da9947b92bccc0e
linZHank <linzhank@gmail.com>
2018-06-29 12:22:34 -0400

    Update and rename installJetsonBot.sh to installBotC2.sh

--- installBotC2.sh
+++ installBotC2.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # Installs the required packages and nodes for the JetsonBot
 cd ~/
-_dir="jetsonbot/src"
+_dir="botc2_ws/src"
 # Check to make sure that the JetsonBot catkin directory exists
-[ ! -d "$_dir" ] && { echo "Error: Directory $_dir not found.\nPlease run createJetsonBotWS.sh to create the JetsonBot catkin workspace."; exit 2; }
+[ ! -d "$_dir" ] && { echo "Error: Directory $_dir not found.\nPlease run createBotC2WS.sh to create the Bot-C2 catkin workspace."; exit 2; }
 # Install TurtleBot from repository
-sudo apt-get install ros-indigo-turtlebot ros-indigo-turtlebot-apps \
-ros-indigo-turtlebot-interactions ros-indigo-rocon-remocon \
-ros-indigo-rocon-qt-library ros-indigo-ar-track-alvar-msgs \
-ros-indigo-collada-urdf -y
+sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps \
+ros-kinetic-turtlebot-interactions ros-kinetic-ar-track-alvar-msgs \
+ros-kinetic-collada-urdf -y
+#(In TK1 tutorial they installed ros-indigo-rocon-remocon ros-indigo-rocon-qt-library, but cannot find these two in ros-kinetic)
 
-cd ~/jetsonbot/src
+cd ~/botc2_ws/src
 git clone https://github.com/jetsonhacks/turtlebot.git
 git clone https://github.com/jetsonhacks/turtlebot_create.git
 git clone https://github.com/jetsonhacks/turtlebot_interactions.git
@@ -19,7 +19,9 @@ git clone https://github.com/jetsonhacks/turtlebot_interactions.git
 git clone https://github.com/jetsonhacks/robot_model.git
 # This fixes an issue with navigation - laser map of RGBD camera was out of bounds
 git clone -b laser_centered https://github.com/jetsonhacks/slam_gmapping.git
-cd ..
-catkin_make
-/bin/echo -e "\e[1;32mJetsonBot Software Installed.\e[0m"
+
+catkin build
+# cd ..
+# catkin_make
+/bin/echo -e "\e[1;32mBot-C2 Software Installed.\e[0m"
 
